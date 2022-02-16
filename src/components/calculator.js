c75b60549aa77d731c397c77c83a4b474c2bf143
unknown <butlermuwo@gmail.com>
2022-02-17 01:05:56 +0200

    Add calculator logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,33 +1,57 @@
-import React from 'react';
+import react from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { calc: ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+'] };
+class Calculator extends react.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
-  render() {
-    const { calc } = this.state;
-    return (
-      <div className="main-container">
-        <div className="display-screen">0</div>
-        <div className="btn-cols">
+  clickHandler = (e) => {
+    if (!e.target.name) return;
 
-          {calc.map((item) => (
-            <div className="btn" key={item}>
-              {item}
-            </div>
+    const { next, total, operation } = calculate(this.state, e.target.name);
+
+    if (next === null && total === null) {
+      this.setState({ next, total: '0', operation });
+    } else {
+      this.setState({ next, total, operation });
+    }
+  };
+
+  render() {
+    const { total, next } = this.state;
+    const btns = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',];
+    return (
+      <div className='main-container'>
+        {next ? (
+          <div className="display-screen">{next}</div>
+        ) : (
+          <div className="display-screen">{total}</div>
+        )}
+        <div className="btn-cols">
+          {btns.map((btnName) => (
+            <button
+              onClick={(e) => this.clickHandler(e)}
+              name={btnName}
+              type="button"
+              className={`btn ${btnName === '0' ? 'btn-zero' : ''} ${
+                btnName === '=' ? 'btn-equals' : ''
+              }`}
+              key={btnName}
+            >
+              {btnName}
+            </button>
           ))}
-        </div>
-        <div className="bottom-cols">
-          <div className="btn">0</div>
-          <div className="btn">.</div>
-          <div className="btn-equals">=</div>
         </div>
       </div>
     );
   }
 }
-
 export default Calculator;
